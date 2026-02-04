7b5c1df755db4799d49c65205bebd41d273c1c3b
Evan Kolokithas <evankolokithas@gmail.com>
2026-02-04 08:23:42 -0600

    Merge exception handling feature branch into main branch

--- SeaOfHands/src/seaOfHands/Game.java
+++ SeaOfHands/src/seaOfHands/Game.java
@@ -1,10 +1,36 @@
 package seaOfHands;
 
+/**
+ * Main Class for the game
+ * 
+ * @author Evan
+ */
 public class Game {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Initail Commit");
-		System.out.println("Team Member Pull and Push");
+		
+		try {
+			//Example Function
+			startGame();
+		}
+		catch(IllegalStateException e) {
+			System.out.println("Game failed to start: " + e);
+		}
+		finally{
+			System.out.println("Game start attempt complete");
+		}
+		
+		
+	}
+	
+	/**
+	 * Throws and exception as an example to implement try catch functionality
+	 * 
+	 * @throws Illegal State Exception
+	 */
+	public static void startGame() {
+		throw new IllegalStateException("Missing Configuration Files");
 	}
 }
+
