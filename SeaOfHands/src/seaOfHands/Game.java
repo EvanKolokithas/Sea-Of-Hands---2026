5ed4235b16eb6b890563840fd2a1410c94201de2
Evan Kolokithas <evankolokithas@gmail.com>
2026-02-10 20:10:10 -0600

    Implemented singleton pattern in game class

--- SeaOfHands/src/seaOfHands/Game.java
+++ SeaOfHands/src/seaOfHands/Game.java
@@ -5,15 +5,52 @@ import org.apache.logging.log4j.Logger;
 
 /**
  * Main Class for the game
+ * Singleton Pattern:
+ * 
+ * Only one instance of the game will be created as well as for world and worldState so
+ * singleton pattern works well
  * 
  * @author Evan
  */
+
 public class Game {
 	
+	private static Game instance;
 	
+	//vars
+	private World world;
+	private WorldState worldState;
+	
+	
+	//constructor
+	private Game() {
+		//temporary starting location
+		world = new World(new Envoirment("temp","temp",true,null));
+		worldState = new WorldState();
+	}
+	
+	public static Game getInstance() {
+		if (instance == null) {
+			instance = new Game();
+		}
+		return instance;
+	}
+	
+	//gettersS
+	public World getWorld() {
+		return world;
+	}
+	
+	public WorldState getWorldState() {
+		return worldState;
+	}
+	
+	//logging
 	final static Logger log = 
 			LogManager.getLogger("Example");
 	
+	
+	//main class
 	public static void main(String[] args) {
 		
 		try {
